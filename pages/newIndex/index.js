664974c59155756064cd0c4fae6e1ce306fa9a89
Qiu Hong <qiuh1016@126.com>
2018-10-19 09:31:05 +0800

    新首页

---
+++ pages/newIndex/index.js
@@ -0,0 +1,73 @@
+// pages/newIndex/index.js
+Page({
+
+  /**
+   * Page initial data
+   */
+  data: {
+    pics: [
+      'https://img.qingdapei.net/home-introduce.png',
+      'https://img.qingdapei.net/home-whyUs.png',
+      'https://img.qingdapei.net/home-collocate.png',
+      'https://img.qingdapei.net/home-userAppraise.png',
+      'https://img.qingdapei.net/home-brand.png',
+    ],
+    index: 0
+  },
+
+  /**
+   * Lifecycle function--Called when page load
+   */
+  onLoad: function (options) {
+
+  },
+
+  /**
+   * Lifecycle function--Called when page is initially rendered
+   */
+  onReady: function () {
+
+  },
+
+  /**
+   * Lifecycle function--Called when page show
+   */
+  onShow: function () {
+
+  },
+
+  /**
+   * Lifecycle function--Called when page hide
+   */
+  onHide: function () {
+
+  },
+
+  /**
+   * Lifecycle function--Called when page unload
+   */
+  onUnload: function () {
+
+  },
+
+  /**
+   * Page event handler function--Called when user drop down
+   */
+  onPullDownRefresh: function () {
+
+  },
+
+  /**
+   * Called when page reach bottom
+   */
+  onReachBottom: function () {
+
+  },
+
+  /**
+   * Called when user click on the top right corner to share
+   */
+  onShareAppMessage: function () {
+
+  }
+})
